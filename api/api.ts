5a20c9ef8d2961d9fc1b621feb7be2af0f7dfe30
R Aswin <aswinr242004@gmail.com>
2024-03-17 22:58:16 +0530

    added files

--- api/api.ts
+++ api/api.ts
@@ -1,11 +1,8 @@
-import express, { Request, Response } from 'express';
-import fs from 'fs'
-import path from 'path'
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const { fileURLToPath } = require('url');
+const { dirname } = require('path');
 
 const app = express();
 const PORT = 5050;
@@ -18,24 +15,15 @@ app.use((req, res, next) => {
     'Access-Control-Allow-Headers',
     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
   )
-  });
-
-interface DataObject {
-  title: string;
-  description: string;
-  primary_color: string;
-  secondary_color: string;
-}
+  next();
+});
 
 const dataFilePath = path.resolve(__dirname, './data', 'data.json');
 
-
-app.get('/card/data', (req: Request, res: Response) => {
+app.get('/card/data', (req, res) => {
   const data = JSON.parse(fs.readFileSync(dataFilePath, 'utf-8'));
   res.json(data);
-
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
