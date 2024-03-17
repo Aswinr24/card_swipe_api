d6f9146e1db1eff298d7187b2bf4da036b594f6c
R Aswin <aswinr242004@gmail.com>
2024-03-17 23:42:11 +0530

    added files

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 const express = require('express')
 const fs = require('fs')
 const path = require('path')
-const { fileURLToPath } = require('url')
-const { dirname } = require('path')
+const cors = require('cors')
 
 const app = express()
 const PORT = 5050
 
 app.use(cors())
 
-const dataFilePath = path.resolve(__dirname, './data', 'data.json')
+// Use __dirname instead of dirname
+const dataFilePath = path.resolve(__dirname, 'data', 'data.json')
 
 app.get('/', (req, res) => {
   console.log('hello')
 })
 
 app.get('/card/data', (req, res) => {
-  const data = JSON.parse(fs.readFileSync(dataFilePath, 'utf-8'))
-  res.json(data)
+  if (fs.existsSync(dataFilePath)) {
+    fs.readFile(dataFilePath, 'utf-8', (err, data) => {
+      if (err) {
+        console.error('Error reading file:', err)
+        res.status(500).send('Internal Server Error')
+      } else {
+        try {
+          const jsonData = JSON.parse(data)
+          res.json(jsonData)
+        } catch (jsonErr) {
+          console.error('Error parsing JSON:', jsonErr)
+          res.status(500).send('Internal Server Error')
+        }
+      }
+    })
+  } else {
+    console.error('Data file not found')
+    res.status(404).send('Data file not found')
+  }
 })
 
 app.listen(PORT, () => {
